07600445ac7760e250fbbee2de9cd463379c5d34
Jeff Irion <jefflirion@users.noreply.github.com>
2019-08-25 01:05:24 +0000

    Working on reconnect test

--- tests/components/androidtv/test_media_player.py
+++ tests/components/androidtv/test_media_player.py
@@ -1,23 +1,31 @@
 """The tests for the androidtv platform."""
+import logging
 import unittest
 from unittest.mock import Mock, patch
 
 # from homeassistant.setup import setup_component
-from homeassistant.components.androidtv.media_player import setup_platform
+from homeassistant.components.androidtv.media_player import (
+    setup_platform,
+    AndroidTVDevice,
+    setup,
+)
 
 # import homeassistant.components.media_player as mp
 # from homeassistant.components.yamaha import media_player as yamaha
 from tests.common import get_test_home_assistant
 
 
-def connect(self):
+_LOGGER = logging.getLogger(__name__)
+
+
+def connect(self, always_log_errors=False):
     """Mimic the `AndroidTV` / `FireTV` connect method."""
     self._adb = True
     self._available = True
     return self._available
 
 
-def connect_fail(self):
+def connect_fail(self, always_log_errors=False):
     """Mimic the `AndroidTV` / `FireTV` connect method."""
     self._adb = False
     self._available = False
@@ -65,6 +73,42 @@ CONFIG_PYTHON_ADB = {
 
 
 class TestAndroidTV(unittest.TestCase):
+    """Test the androidtv media player for an Android TV device."""
+
+    def setUp(self):
+        """Set up an `AndroidTVDevice` media player."""
+        with patch("androidtv.basetv.BaseTV.connect", connect), patch(
+            "androidtv.basetv.BaseTV._adb_shell_python_adb", return_value=None
+        ):
+            atv = setup("127.0.0.1:5555", device_class="androidtv")
+            self.atv = AndroidTVDevice(atv, "Fake Android TV", {}, None, None)
+
+    '''def test_reconnect(self):
+        """Test the update method."""
+        with self.assertLogs(level=logging.ERROR) as logs:
+            with patch("androidtv.basetv.BaseTV.connect", connect_fail), patch("androidtv.basetv.BaseTV._adb_shell_python_adb", adb_shell_python_adb_error):
+                self.atv.update()
+                for _ in range(5):
+                    _LOGGER.critical(self.atv.available)
+                    self.atv.update()
+                    self.atv.aftv.connect()
+                    self.assertTrue(not self.atv.aftv.available)
+                    self.assertTrue(not self.atv.available)
+            with patch.object(self.atv.aftv, "connect", connect_fail),
+                patch.object(self.atv.aftv, "adb_shell", adb_shell_python_adb_error):
+                #with patch("androidtv.basetv.BaseTV.connect", connect_fail), patch(
+                #    "androidtv.basetv.BaseTV._adb_shell_python_adb", adb_shell_python_adb_error
+                #):
+                for _ in range(5):
+                    _LOGGER.critical(self.atv.available)
+                    self.atv.update()
+                    self.assertTrue(not self.atv.available)
+
+        #self.assertEqual(logs, [])
+        #self.assertTrue(False)'''
+
+
+class TestAndroidTV2(unittest.TestCase):
     """Test the androidtv media player for an Android TV device."""
 
     def setUp(self):
