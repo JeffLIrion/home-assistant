bc3fb274e1cd399e0cf92540b1ae83c7d57db780
Jeff Irion <jefflirion@users.noreply.github.com>
2019-12-13 03:29:31 +0000

    Add custom input_number and cast_volume_tracker

--- homeassistant/components/input_number/__init__.py
+++ homeassistant/components/input_number/__init__.py
@@ -6,14 +6,23 @@ import voluptuous as vol
 from homeassistant.const import (
     ATTR_MODE,
     ATTR_UNIT_OF_MEASUREMENT,
+    CONF_ENTITY_ID,
     CONF_ICON,
+    CONF_ICON_TEMPLATE,
     CONF_MODE,
     CONF_NAME,
+    CONF_VALUE_TEMPLATE,
+    EVENT_HOMEASSISTANT_START,
+    MATCH_ALL,
     SERVICE_RELOAD,
 )
+from homeassistant.core import callback
+from homeassistant.exceptions import TemplateError
 import homeassistant.helpers.config_validation as cv
 from homeassistant.helpers.entity_component import EntityComponent
+from homeassistant.helpers.event import async_track_state_change
 from homeassistant.helpers.restore_state import RestoreEntity
+from homeassistant.helpers.script import Script
 import homeassistant.helpers.service
 
 _LOGGER = logging.getLogger(__name__)
@@ -54,6 +63,29 @@ def _cv_input_number(cfg):
     return cfg
 
 
+# (Start) Template Number
+CONF_SET_VALUE_SCRIPT = "set_value_script"
+CONF_VALUE_CHANGED_SCRIPT = "value_changed_script"
+SERVICE_SET_VALUE_NO_SCRIPT = "set_value_no_script"
+
+
+def _cv_template_number(cfg):
+    """Configure validation helper for template number (voluptuous)."""
+    _cv_input_number(cfg)
+
+    if CONF_VALUE_TEMPLATE in cfg and CONF_SET_VALUE_SCRIPT not in cfg:
+        raise vol.Invalid(
+            "{} cannot be provided without {}".format(
+                CONF_VALUE_TEMPLATE, CONF_SET_VALUE_SCRIPT
+            )
+        )
+
+    return cfg
+
+
+# (End) Template Number
+
+
 CONFIG_SCHEMA = vol.Schema(
     {
         DOMAIN: cv.schema_with_slug_keys(
@@ -71,8 +103,15 @@ CONFIG_SCHEMA = vol.Schema(
                     vol.Optional(CONF_MODE, default=MODE_SLIDER): vol.In(
                         [MODE_BOX, MODE_SLIDER]
                     ),
+                    # (Start) Template Number
+                    vol.Optional(CONF_VALUE_TEMPLATE): cv.template,
+                    vol.Optional(CONF_SET_VALUE_SCRIPT): cv.SCRIPT_SCHEMA,
+                    vol.Optional(CONF_ENTITY_ID): cv.entity_ids,
+                    vol.Optional(CONF_ICON_TEMPLATE): cv.template,
+                    vol.Optional(CONF_VALUE_CHANGED_SCRIPT): cv.SCRIPT_SCHEMA,
+                    # (End) Template Number
                 },
-                _cv_input_number,
+                _cv_template_number,
             )
         )
     },
@@ -86,14 +125,14 @@ async def async_setup(hass, config):
     """Set up an input slider."""
     component = EntityComponent(_LOGGER, DOMAIN, hass)
 
-    entities = await _async_process_config(config)
+    entities = await _async_process_config(hass, config)
 
     async def reload_service_handler(service_call):
         """Remove all entities and load new ones from config."""
         conf = await component.async_prepare_reload()
         if conf is None:
             return
-        new_entities = await _async_process_config(conf)
+        new_entities = await _async_process_config(hass, conf)
         if new_entities:
             await component.async_add_entities(new_entities)
 
@@ -111,6 +150,14 @@ async def async_setup(hass, config):
         "async_set_value",
     )
 
+    # (Start) Template Number
+    component.async_register_entity_service(
+        SERVICE_SET_VALUE_NO_SCRIPT,
+        {vol.Required(ATTR_VALUE): vol.Coerce(float)},
+        "async_set_value_no_script",
+    )
+    # (End) Template Number
+
     component.async_register_entity_service(SERVICE_INCREMENT, {}, "async_increment")
 
     component.async_register_entity_service(SERVICE_DECREMENT, {}, "async_decrement")
@@ -120,7 +167,7 @@ async def async_setup(hass, config):
     return True
 
 
-async def _async_process_config(config):
+async def _async_process_config(hass, config):
     """Process config and create list of entities."""
     entities = []
 
@@ -133,6 +180,51 @@ async def _async_process_config(config):
         icon = cfg.get(CONF_ICON)
         unit = cfg.get(ATTR_UNIT_OF_MEASUREMENT)
         mode = cfg.get(CONF_MODE)
+
+        # (Start) Template Number
+        if CONF_SET_VALUE_SCRIPT in cfg:
+            icon_template = cfg.get(CONF_ICON_TEMPLATE)
+            set_value_script = cfg.get(CONF_SET_VALUE_SCRIPT)
+            value_template = cfg.get(CONF_VALUE_TEMPLATE)
+            value_changed_script = cfg.get(CONF_VALUE_CHANGED_SCRIPT)
+
+            # setup the entity ID's for the template
+            template_entity_ids = set()
+            if value_template is not None:
+                temp_ids = value_template.extract_entities()
+                if str(temp_ids) != MATCH_ALL:
+                    template_entity_ids |= set(temp_ids)
+
+            if icon_template is not None:
+                icon_ids = icon_template.extract_entities()
+                if str(icon_ids) != MATCH_ALL:
+                    template_entity_ids |= set(icon_ids)
+
+            entity_ids = cfg.get(CONF_ENTITY_ID, template_entity_ids)
+
+            # Template Number
+            entities.append(
+                TemplateNumber(
+                    object_id,
+                    name,
+                    initial,
+                    minimum,
+                    maximum,
+                    step,
+                    icon,
+                    icon_template,
+                    unit,
+                    mode,
+                    hass,
+                    value_template,
+                    set_value_script,
+                    entity_ids,
+                    value_changed_script,
+                )
+            )
+
+            continue
+        # (End) Template Number
 
         entities.append(
             InputNumber(
@@ -253,3 +345,194 @@ class InputNumber(RestoreEntity):
             return
         self._current_value = new_value
         await self.async_update_ha_state()
+
+    async def async_set_value_no_script(self, value):
+        """Set new value."""
+        num_value = float(value)
+        if num_value < self._minimum or num_value > self._maximum:
+            _LOGGER.warning(
+                "Invalid value: %s (range %s - %s)",
+                num_value,
+                self._minimum,
+                self._maximum,
+            )
+            return
+        self._current_value = num_value
+        await self.async_update_ha_state()
+
+
+class TemplateNumber(InputNumber):
+    """Representation of a slider with template functionality."""
+
+    def __init__(
+        self,
+        object_id,
+        name,
+        initial,
+        minimum,
+        maximum,
+        step,
+        icon,
+        icon_template,
+        unit,
+        mode,
+        hass,
+        value_template,
+        set_value_script,
+        entity_ids,
+        value_changed_script,
+    ):
+        """Initialize a template number."""
+        super().__init__(
+            object_id, name, initial, minimum, maximum, step, icon, unit, mode
+        )
+        self.hass = hass
+        self._entities = entity_ids
+
+        # template
+        self._value_template = value_template
+        if self._value_template is not None:
+            self._value_template.hass = self.hass
+
+        # icon template
+        self._icon_template = icon_template
+        if self._icon_template is not None:
+            self._icon_template.hass = self.hass
+
+        # set_value_script
+        if set_value_script:
+            self._set_value_script = Script(hass, set_value_script)
+        else:
+            self._set_value_script = None
+
+        # value_changed_script
+        if value_changed_script:
+            self._value_changed_script = Script(hass, value_changed_script)
+        else:
+            self._value_changed_script = None
+
+    async def async_added_to_hass(self):
+        """Run when entity about to be added to hass and register callbacks."""
+
+        @callback
+        def template_number_state_listener(entity, old_state, new_state):
+            """Handle target device state changes."""
+            self.async_schedule_update_ha_state(True)
+
+        @callback
+        def template_number_startup(event):
+            """Listen for state changes."""
+            if self._entities:
+                async_track_state_change(
+                    self.hass, self._entities, template_number_state_listener
+                )
+
+            self.async_schedule_update_ha_state(True)
+
+        self.hass.bus.async_listen_once(
+            EVENT_HOMEASSISTANT_START, template_number_startup
+        )
+
+        await super().async_added_to_hass()
+        if self._current_value is not None:
+            return
+
+        state = await self.async_get_last_state()
+        value = state and float(state.state)
+
+        # Check against None because value can be 0
+        if value is not None and self._minimum <= value <= self._maximum:
+            self._current_value = value
+        else:
+            self._current_value = self._minimum
+
+    async def async_set_value(self, value):
+        """Set new value."""
+        num_value = float(value)
+        if num_value < self._minimum or num_value > self._maximum:
+            _LOGGER.warning(
+                "Invalid value: %s (range %s - %s)",
+                num_value,
+                self._minimum,
+                self._maximum,
+            )
+            return
+        self._current_value = num_value
+
+        if self._set_value_script:
+            await self._set_value_script.async_run(
+                {"value": self._current_value}, context=self._context
+            )
+        await self.async_update_ha_state()
+
+    async def async_increment(self):
+        """Increment value."""
+        new_value = self._current_value + self._step
+        if new_value > self._maximum:
+            _LOGGER.warning(
+                "Invalid value: %s (range %s - %s)",
+                new_value,
+                self._minimum,
+                self._maximum,
+            )
+            return
+        self._current_value = new_value
+
+        if self._set_value_script:
+            await self._set_value_script.async_run(
+                {"value": self._current_value}, context=self._context
+            )
+
+        await self.async_update_ha_state()
+
+    async def async_decrement(self):
+        """Decrement value."""
+        new_value = self._current_value - self._step
+        if new_value < self._minimum:
+            _LOGGER.warning(
+                "Invalid value: %s (range %s - %s)",
+                new_value,
+                self._minimum,
+                self._maximum,
+            )
+            return
+        self._current_value = new_value
+
+        if self._set_value_script:
+            await self._set_value_script.async_run(
+                {"value": self._current_value}, context=self._context
+            )
+
+        await self.async_update_ha_state()
+
+    async def async_update(self):
+        """Update the state from the template."""
+        if self._value_template:
+            try:
+                value = self._value_template.async_render()
+                if value not in ["None", "unknown"] and self._current_value != float(
+                    value
+                ):
+                    self._current_value = float(value)
+
+                    if self._value_changed_script:
+                        await self._value_changed_script.async_run(
+                            {"value": self._current_value}, context=self._context
+                        )
+
+            except TemplateError as ex:
+                _LOGGER.error(ex)
+
+        if self._icon_template:
+            try:
+                setattr(self, "_icon", self._icon_template.async_render())
+            except TemplateError as ex:
+                if ex.args and ex.args[0].startswith(
+                    "UndefinedError: 'None' has no attribute"
+                ):
+                    # Common during HA startup - so just a warning
+                    _LOGGER.warning(
+                        "Could not render icon template for %s, "
+                        "the state is unknown.",
+                        self._name,
+                    )
