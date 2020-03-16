766136eda534cdbc2204fb017a1cb4ac4e3f26d9
Jeff Irion <jefflirion@users.noreply.github.com>
2020-03-16 02:46:51 +0000

    Configure all media players and cast volume trackers

--- tests/components/cast_volume_tracker/test_cast_volume_tracker.py
+++ tests/components/cast_volume_tracker/test_cast_volume_tracker.py
@@ -3,31 +3,41 @@
 import os
 
 from homeassistant.components.cast_volume_tracker import DOMAIN as CVT_DOMAIN
-from homeassistant.components.media_player.const import DOMAIN
+from homeassistant.components.media_player.const import DOMAIN as MP_DOMAIN
 from homeassistant.const import STATE_IDLE, STATE_OFF
 from homeassistant.setup import async_setup_component
 from homeassistant.util.yaml.loader import load_yaml
 
 PWD = os.path.dirname(__file__)
 
+MEDIA_PLAYERS = [
+    "all_my_speakers",
+    "bedroom_mini",
+    "bedroom_speakers",
+    "computer_speakers",
+    "kitchen_home",
+    "kitchen_speakers",
+    "living_room_display",
+    "living_room_speakers",
+    "main_speakers",
+]
+
+CAST_MOCK_CONFIG = {MP_DOMAIN: load_yaml(PWD + "/media_players.yaml")}
 CAST_VOLUME_TRACKER_CONFIG = {CVT_DOMAIN: load_yaml(PWD + "/cast_volume_trackers.yaml")}
-CAST_MOCK_CONFIG = {DOMAIN: load_yaml(PWD + "/media_players.yaml")}
 
 
 async def test_setup(hass):
     """Test that a `cast_mock` media player can be created."""
-    assert await async_setup_component(hass, DOMAIN, CAST_MOCK_CONFIG)
+    assert await async_setup_component(hass, MP_DOMAIN, CAST_MOCK_CONFIG)
     assert await async_setup_component(hass, CVT_DOMAIN, CAST_VOLUME_TRACKER_CONFIG)
 
-    for entity_id in [
-        "media_player.bedroom_speakers",
-        "media_player.computer_speakers",
-    ]:
-        state = hass.states.get(entity_id)
+    for media_player in MEDIA_PLAYERS:
+        mp_entity_id = "media_player.{}".format(media_player)
+        state = hass.states.get(mp_entity_id)
         assert state is not None
         assert state.state == STATE_OFF
         assert state.state != STATE_IDLE
 
-        cvt_entity_id = entity_id.replace("media_player", "cast_volume_tracker")
+        cvt_entity_id = "cast_volume_tracker.{}".format(media_player)
         state = hass.states.get(cvt_entity_id)
         assert state is not None
